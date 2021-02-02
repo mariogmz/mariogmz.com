db4f6cb4576b4fca37a4787d98c78670f6c5b4f3
Mario Gómez <mariogomezmtz@gmail.com>
2021-02-02 14:06:49 -0600

    Introduce Hotwire

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.0.0"
 
 gem "devise"
+gem "hotwire-rails"
 gem "rails", "~> 6.1.1"
 gem "pg"
 gem "puma", "~> 5.0"
--- app/validators/page_validator.rb
+++ app/validators/page_validator.rb
@@ -5,7 +5,7 @@ class PageValidator < ActiveModel::Validator
 
   private
     def is_the_only_one_active(record)
-      if record.active && record.profile.pages.active.any?
+      if record.active && record.profile.pages.active.where.not(id: record.id).any?
         record.errors.add(:active, :not_unique)
       end
     end
