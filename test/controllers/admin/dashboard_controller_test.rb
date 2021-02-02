629b82e6c95b2107d04e5869f9c4d020b76df1a6
Mario Gómez <mariogomezmtz@gmail.com>
2021-02-02 12:31:28 -0600

    Add test for new user profile creation

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -3,5 +3,17 @@ require_relative "./controller_test"
 
 module Admin
   class DashboardControllerTest < ControllerTest
+    test "creates a profile automatically if there's not one created" do
+      sign_out(@user)
+      new_user = User.create(email: "new_user@test.com", password: "test123!")
+      sign_in(new_user)
+
+      assert_nil(new_user.profile)
+
+      get admin_dashboard_url
+
+      assert_response :success
+      assert_not_nil(new_user.profile)
+    end
   end
 end
