408690c7ecfd0cbf2d2fad442a8c429a325b0ef5
Mario Gómez <mariogomezmtz@gmail.com>
2021-02-23 18:17:13 -0600

    Add stimulus-rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
-import Rails from "@rails/ujs";
-import "@hotwired/turbo-rails";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "controllers/hello_controller";
-import "fonts/montserrat";
+import Rails from '@rails/ujs';
+import '@hotwired/turbo-rails';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
+import 'controllers';
+import 'fonts/montserrat';
 
 Rails.start();
 ActiveStorage.start();
 
 require.context('../icons', true);
 
-require("stylesheets/application.css");
+require('stylesheets/application.css');
