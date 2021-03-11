8605cd42bf424f21c4297d219e76132257bbad66
Mario Gómez <mariogomezmtz@gmail.com>
2021-03-11 10:26:46 -0600

    Broadcast language model updates to turbo streams

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,7 @@
 class Language < ApplicationRecord
   belongs_to :profile
+
+  after_create_commit { broadcast_prepend_to :languages, partial: "admin/languages/language" }
+  after_update_commit { broadcast_replace_to :languages, partial: "admin/languages/language" }
+  after_destroy_commit { broadcast_remove_to :languages }
 end
